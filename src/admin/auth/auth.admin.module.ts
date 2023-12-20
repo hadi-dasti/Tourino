8ed5a2cfe6b464e90f17f00ff0c654ea7f19bf0a
hadi dasti <hadidasti98@hotmail.com>
2023-12-20 20:07:15 +0330

    update auth.admin.module

--- src/admin/auth/auth.admin.module.ts
+++ src/admin/auth/auth.admin.module.ts
@@ -1,19 +1,18 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthAdminEntity } from './auth.admin.entity';
 import { AuthAdminService } from './auth.admin.service';
 import { AuthAdminController } from './auth.admin.controller';
-
+import { RecoveryPasswordMobileModule } from './recovery-password-mobile/recovery.password.mobile.module';
 
 @Module({
-    // Import the TypeOrmModule with the AuthAdminEntity for feature
-    imports: [TypeOrmModule.forFeature([AuthAdminEntity])],
-    // Provide the AuthAdminService as a provider
-    providers: [AuthAdminService],
-    // Use the AuthAdminController as a controller
-    controllers:[AuthAdminController]
+  // Import the TypeOrmModule with the AuthAdminEntity for feature
+  imports: [RecoveryPasswordMobileModule,
+    TypeOrmModule.forFeature([AuthAdminEntity]),  
+  ],
+  // Provide the AuthAdminService as a provider
+  providers: [AuthAdminService],
+  // Use the AuthAdminController as a controller
+  controllers: [AuthAdminController],
 })
-
-
-
-export class authAminModule { }
+export class authAminModule {}
