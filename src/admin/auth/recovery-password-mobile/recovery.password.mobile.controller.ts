2248cd0c8fac4c5a8df96c122d0ad48ef746de77
hadi dasti <hadidasti98@hotmail.com>
2023-12-23 19:14:43 +0330

    update recovery.password.service and recovery.password.controller

--- src/admin/auth/recovery-password-mobile/recovery.password.mobile.controller.ts
+++ src/admin/auth/recovery-password-mobile/recovery.password.mobile.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Body, Post, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  NotFoundException,
+  Put,
+  Param,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { RecoveryPasswordMobileService } from './recovery.password.mobile.service';
 import { AuthAdminEntity } from '../auth.admin.entity';
 import { AuthAdminDto } from '../auth.admin.dto';
+
 
 
 @Controller('/api/v1/admin/auth-admin/recovery-password-password')
@@ -11,7 +20,7 @@ export class RecoveryPasswordMobileController {
   ) {}
 
   @Post('/initial-mobile')
-  public async initialMobileAdmin(
+  async initialMobileAdmin(
     @Body() authAdminDto: AuthAdminDto,
   ): Promise<AuthAdminEntity> {
     try {
@@ -23,11 +32,45 @@ export class RecoveryPasswordMobileController {
       if (!newInitialMobileAdmin) {
         throw new NotFoundException('Error Not Found');
       }
-      
+
       // TODO: Send the codeOTP to the user's mobile number (e.g., via SMS)
+
       return newInitialMobileAdmin;
     } catch (err) {
       console.log(err);
+      throw new InternalServerErrorException('Failed to initialize mobile admin recovery process');
+    }
+  }
+
+  @Post('/verify-codeOtp')
+  async verifyCodeOtpMobile(@Body() authAdminDto: AuthAdminDto): Promise<void> {
+    try {
+      await this.RecoveryPasswordMobileService.verifyMobileAdmin(authAdminDto);
+    } catch (err) {
+      throw new InternalServerErrorException(
+        'Verify Password failed')
+    }
+  }
+
+  @Put('/new-password/:adminId')
+  async changePasswordAdmin(
+    @Body() authAdminDto: AuthAdminDto,
+    @Param('adminId') id: string,
+  ): Promise<AuthAdminEntity> {
+    try {
+      const newPassword =
+        await this.RecoveryPasswordMobileService.changePasswordAdmin(
+          authAdminDto,
+          id,
+        );
+      if (!newPassword) {
+        throw new NotFoundException('Password change failed');
+      }
+      return newPassword;
+    } catch (err) {
+      throw new InternalServerErrorException(
+        'Password change operation failed',
+      );
     }
   }
 }
--- src/admin/auth/recovery-password-mobile/recovery.password.mobile.service.ts
+++ src/admin/auth/recovery-password-mobile/recovery.password.mobile.service.ts
@@ -1,5 +1,6 @@
 import {
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -44,6 +45,64 @@ export class RecoveryPasswordMobileService {
       
     } catch (err) {
       console.log(err);
+      throw new InternalServerErrorException('Failed to initialize mobile admin recovery process')
+    }
+  }
+
+  // Verifies the code OTP for admin's mobile password recovery
+  public async verifyMobileAdmin(authAdminDto: AuthAdminDto): Promise<void>{
+    
+    const { codeOtp } = authAdminDto;
+
+    try {
+
+      const verifyCodeOtpMobile = await this.authAdminEntity.findOneBy({ codeOtp });
+
+      if (!verifyCodeOtpMobile) {
+        console.log('Error: CodeOTP not found');
+        throw new NotFoundException('CodeOTP not found');
+      }
+
+      if (verifyCodeOtpMobile.codeOtp !== codeOtp) {
+        console.log('Error: CodeOTP does not match');
+        throw new UnauthorizedException('Invalid CodeOTP');
+      }
+
+      // CodeOTP verification successful
+      console.log('CodeOTP verification successful');
+      
+    } catch (err) {
+      console.log(err)
+      throw new InternalServerErrorException('Failed to verify mobile admin recovery OTP');
+    }
+  }
+
+  // Changes the password for admin's mobile password recovery
+  public async changePasswordAdmin(authAdminDto: AuthAdminDto, id: string): Promise<AuthAdminEntity>{
+    
+    const { password } = authAdminDto;
+
+    try {
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const existingAdmin = await this.authAdminEntity.findOneBy({ id });
+
+       if (!existingAdmin) {
+         console.log('Error: Admin not found with the provided id');
+         throw new NotFoundException('Admin not found with the provided id');
+      }
+      
+      existingAdmin.password = hashedPassword;
+      existingAdmin.codeOtp = "";
+
+      const newPasswordAdmin = await this.authAdminEntity.save(existingAdmin);
+      return newPasswordAdmin;
+
+    } catch (err) {
+      console.log(err)
+      throw new InternalServerErrorException('Failed to change admin password');
     }
   }
 }
