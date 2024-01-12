7702f9da36ef3ef46231c3a47b58461c2a02a423
hadi dasti <hadidasti98@hotmail.com>
2024-01-12 20:06:27 +0330

    modify main.ts and add setup swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerOptions } from './swagger.config';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  const app = await NestFactory.create(AppModule,{abortOnError:true});
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
   await app.listen(4000);
 }
 bootstrap();
