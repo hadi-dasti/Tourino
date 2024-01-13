48f635fe0d36ecdc694123c01414246bf35cdda3
hadi dasti <hadidasti98@hotmail.com>
2024-01-13 22:29:29 +0330

    modify module client

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthClientModule } from './auth-client/auth.client.module';
+import { PackagesClientModule } from './packages/package.client.module';
 
 @Module({
-  imports: [AuthClientModule],
+  imports: [AuthClientModule, PackagesClientModule],
 })
 export class ClientModule {}
