5af995446b310b6fbaab249fd771066b0080e8e6
hadi dasti <hadidasti98@hotmail.com>
2024-01-28 00:35:13 +0330

    modify in files

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthClientModule } from './auth-client/auth.client.module';
 import { PackagesClientModule } from './packages/package.client.module';
+import { DashboardClientModule } from './dashboard-client/dashboard.client.module';
 
 @Module({
-  imports: [AuthClientModule, PackagesClientModule],
+  imports: [AuthClientModule, PackagesClientModule, DashboardClientModule],
 })
 export class ClientModule {}
--- src/client/dashboard-client/dashboard.client.module.ts
+++ src/client/dashboard-client/dashboard.client.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { RegistrationManagementModule } from './registration-management/registration.management.module';
 
-@Module({})
+@Module({
+  imports: [RegistrationManagementModule],
+})
 export class DashboardClientModule {}
