33c82ddfd8ddc50c6fbe91bcc06c586882b868b7
hadi dasti <hadidasti98@hotmail.com>
2024-02-01 20:45:22 +0330

    update program in dashboard

--- src/client/dashboard-client/program-management/program.management.controller.ts
+++ src/client/dashboard-client/program-management/program.management.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpCode, Post,HttpStatus, Body } from "@nestjs/common";
+import { Controller, HttpCode, Post,HttpStatus, Body, Get, HttpException } from "@nestjs/common";
 import { ProgramManagementService } from "./program.management.service";
 import { ProgramDto } from "./program.management.dto";
 import { ProgramManagementEntity } from "./program.maagement.entity";
@@ -6,9 +6,7 @@ import { ProgramManagementEntity } from "./program.maagement.entity";
 
 @Controller('/api/v1/client/dashboard/program-management')
 export class ProgramManagementController {
-  constructor(
-    private readonly programManagementService: ProgramManagementService,
-  ) {}
+  constructor(private readonly programManagementService: ProgramManagementService) {}
 
   @Post('/add-program')
   @HttpCode(HttpStatus.CREATED)
@@ -17,6 +15,17 @@ export class ProgramManagementController {
       return await this.programManagementService.addProgram(ProgramDto);
     } catch (err) {
       HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+  }
+
+  @Get('/get-all-programs')
+  @HttpCode(HttpStatus.OK)
+  async getAllPrograms(): Promise<ProgramManagementEntity[]> {
+    try {
+      return await this.programManagementService.getAllPrograms();
+    } catch (err) {
+      console.error('Error fetching programs:', err);
+      throw new HttpException('Failed to fetch programs.', HttpStatus.BAD_REQUEST)
     }
   }
 }
--- src/client/dashboard-client/program-management/program.management.service.ts
+++ src/client/dashboard-client/program-management/program.management.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import {ProgramManagementEntity,ProgramStatus} from './program.maagement.entity';
@@ -7,23 +7,24 @@ import { ProgramDto } from './program.management.dto';
 
 @Injectable()
 export class ProgramManagementService {
-  constructor(@InjectRepository(ProgramManagementEntity)
-  private programRepository: Repository<ProgramManagementEntity>) { }
-  
-// add program
+  constructor(
+    @InjectRepository(ProgramManagementEntity)
+    private programRepository: Repository<ProgramManagementEntity>,
+  ) {}
+
+  // add program
   public async addProgram(programDto: ProgramDto): Promise<ProgramManagementEntity> {
-     const {
-       programName,
-       programTime,
-       programStatus,
-       price,
-       registeredPeople,
-       popularity,
-       operationIcons,
+    const {
+      programName,
+      programTime,
+      programStatus,
+      price,
+      registeredPeople,
+      popularity,
+      operationIcons,
     } = programDto;
-    
+
     try {
-     
       // Check if programStatus is empty or undefined
       if (!programStatus || programStatus.trim() === '') {
         console.error('Program status is required');
@@ -33,7 +34,9 @@ export class ProgramManagementService {
         );
       }
 
-      if (!Object.values(ProgramStatus).includes(programStatus as ProgramStatus)) {
+      if (
+        !Object.values(ProgramStatus).includes(programStatus as ProgramStatus)
+      ) {
         console.error('Invalid program status');
         throw new HttpException(
           'Invalid program status',
@@ -58,10 +61,44 @@ export class ProgramManagementService {
       }
 
       return program;
-      
     } catch (err) {
       console.error('Internal Server Error:', err.message);
-      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  //get all programs
+  public async getAllPrograms(): Promise<ProgramManagementEntity[]> {
+
+    try {
+
+      const allPrograms = await this.programRepository.find({
+        select: {
+          id: true,
+          programName: true,
+          programTime: true,
+          programStatus: true,
+          price:true
+        },
+        order: {
+          programName: 'ASC',
+        },
+        take: 10,
+        cache: true,
+      });
+
+      if (!allPrograms.length) {
+         throw new NotFoundException('No programs found.');
+      }
+
+      return allPrograms;
+
+    } catch (err) {
+      console.log(err)
+      throw new InternalServerErrorException('Failed to fetch programs.');
     }
   }
 }
