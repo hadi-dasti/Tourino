ca4a1179a7e844b83cb36df9a8a7bda4407c1b8c
hadi dasti <hadidasti98@hotmail.com>
2024-02-02 19:20:28 +0330

    update and add method put to controller of programs

--- src/client/dashboard-client/program-management/program.management.controller.ts
+++ src/client/dashboard-client/program-management/program.management.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpCode, Post,HttpStatus, Body, Get, HttpException } from "@nestjs/common";
+import { Controller, HttpCode, Post,HttpStatus, Body, Get, HttpException, Put, Param } from "@nestjs/common";
 import { ProgramManagementService } from "./program.management.service";
 import { ProgramDto } from "./program.management.dto";
 import { ProgramManagementEntity } from "./program.maagement.entity";
@@ -6,11 +6,15 @@ import { ProgramManagementEntity } from "./program.maagement.entity";
 
 @Controller('/api/v1/client/dashboard/program-management')
 export class ProgramManagementController {
-  constructor(private readonly programManagementService: ProgramManagementService) {}
+  constructor(
+    private readonly programManagementService: ProgramManagementService,
+  ) {}
 
   @Post('/add-program')
   @HttpCode(HttpStatus.CREATED)
-  async addProgram(@Body() ProgramDto: ProgramDto): Promise<ProgramManagementEntity> {
+  async addProgram(
+    @Body() ProgramDto: ProgramDto,
+  ): Promise<ProgramManagementEntity> {
     try {
       return await this.programManagementService.addProgram(ProgramDto);
     } catch (err) {
@@ -25,7 +29,26 @@ export class ProgramManagementController {
       return await this.programManagementService.getAllPrograms();
     } catch (err) {
       console.error('Error fetching programs:', err);
-      throw new HttpException('Failed to fetch programs.', HttpStatus.BAD_REQUEST)
+      throw new HttpException(
+        'Failed to fetch programs.',
+        HttpStatus.BAD_REQUEST,
+      );
     }
   }
+
+  @Put('/update-all-program/:id')
+  async updateAllProgram(
+    @Param('id') id: string,
+    @Body() ProgramDto: ProgramDto): Promise<any> {
+    try {
+      return await this.programManagementService.updateProgram(id, ProgramDto);
+    } catch (err) {
+      console.error('Error updating program:', err);
+      throw new HttpException(
+        'Failed to update program',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+   }
+  
 }
