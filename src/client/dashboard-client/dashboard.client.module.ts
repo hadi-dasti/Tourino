98b05a648d7065adef80214fccd53bd600a3e86c
hadi dasti <hadidasti98@hotmail.com>
2024-02-04 23:14:45 +0330

    update module dashboard client

--- src/client/dashboard-client/dashboard.client.module.ts
+++ src/client/dashboard-client/dashboard.client.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RegistrationManagementModule } from './registration-management/registration.management.module';
 import { ProgramManagementModule } from './program-management/program.management.module';
+import { UserManagementModule } from './user-management/user.management.module';
+
 
 @Module({
   imports: [
     RegistrationManagementModule,
-    ProgramManagementModule
+    ProgramManagementModule,
+    UserManagementModule
   ],
 })
 export class DashboardClientModule {}
