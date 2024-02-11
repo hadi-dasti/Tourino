b6fff44c4b084925df857dc1dea01fc6ef499867
hadi dasti <hadidasti98@hotmail.com>
2024-02-11 23:43:03 +0330

    add update user in dashboard of client in service and controller

--- src/client/dashboard-client/user-management/user.management.controller.ts
+++ src/client/dashboard-client/user-management/user.management.controller.ts
@@ -1,28 +1,52 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, InternalServerErrorException, Get } from "@nestjs/common";
+import { Controller, Post, Body, HttpCode, HttpStatus, InternalServerErrorException, Get, Patch, Param, NotFoundException } from "@nestjs/common";
 import { UserManagementService } from './user.management.service';
 import { UserManagementDto } from './user.management.dto';
 import { UserManagementEntity } from './user.management.entity';
 
-@Controller('/api/v1/client/dashboard/user-management')
+@Controller("/api/v1/client/dashboard/user-management")
 export class UserManagementController {
   constructor(private readonly userManagementService: UserManagementService) {}
-  @Post('/add-user')
+  @Post("/add-user")
   @HttpCode(HttpStatus.CREATED)
-  async addUser(@Body() userDto: UserManagementDto): Promise<UserManagementEntity> {
+  async addUser(
+    @Body() userDto: UserManagementDto
+  ): Promise<UserManagementEntity> {
     try {
-        return await this.userManagementService.createAddUser(userDto);
+      return await this.userManagementService.createAddUser(userDto);
     } catch (err) {
-        throw new InternalServerErrorException('Failed to add user');
-      }
-  }
-
-  @Get('/get-all-users')
-  @HttpCode(HttpStatus.OK)
-  async getAllUsers(): Promise<UserManagementEntity[]>{
-    try {
-        return await this.userManagementService.getManagementAllUsers();
-    } catch (err) {
-        throw new InternalServerErrorException('Failed to retrieve all users');
+      throw new InternalServerErrorException("Failed to add user");
     }
   }
+
+  @Get("/get-all-users")
+  @HttpCode(HttpStatus.OK)
+  async getAllUsers(): Promise<UserManagementEntity[]> {
+    try {
+      return await this.userManagementService.getManagementAllUsers();
+    } catch (err) {
+      throw new InternalServerErrorException("Failed to retrieve all users");
+    }
+  }
+
+  @Patch("/update-user/:id")
+  async updateUser(
+    @Param('id') id: string,
+    @Body()userDto: UserManagementDto): Promise<UserManagementEntity | any> {
+    try {
+        const checkUser = await this.userManagementService.updateManagementUser(
+          id,
+          userDto,
+        );
+
+        if (!checkUser) {
+          throw new NotFoundException(`User with ID ${id} not found`);
+        }
+      return checkUser;
+      
+      } catch (err) {
+        throw new InternalServerErrorException(
+          `Failed to update user: ${err.message}`,
+        );
+    }
+  };
 }
--- src/client/dashboard-client/user-management/user.management.service.ts
+++ src/client/dashboard-client/user-management/user.management.service.ts
@@ -4,82 +4,129 @@ import { Repository } from 'typeorm';
 import { UserManagementEntity } from './user.management.entity';
 import { UserManagementDto } from './user.management.dto';
 import { SituationUser, GenderUser, RegistrationStatusUser } from './uesr.management.enum';
-import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class UserManagementService {
   constructor(
     @InjectRepository(UserManagementEntity)
-    private readonly userManagementRepository: Repository<UserManagementEntity>) {}
+    private readonly userManagementRepository: Repository<UserManagementEntity>,
+  ) {}
 
   // registery user in app by client
-    public async createAddUser(userDto: UserManagementDto): Promise<UserManagementEntity> {
-      
-      const {
-          name,
-          lastName,
-          situation,
-          paymentAmount,
-          nationalCode,
-          programs,
-          province,
-          gender,
-          registrationStatus
-      } = userDto;
+  public async createAddUser(
+    userDto: UserManagementDto,
+  ): Promise<UserManagementEntity> {
+    const {
+      name,
+      lastName,
+      situation,
+      paymentAmount,
+      nationalCode,
+      programs,
+      province,
+      gender,
+      registrationStatus,
+    } = userDto;
 
-        try { 
-          
+    try {
       if (!Object.values(SituationUser).includes(situation)) {
-          throw new BadRequestException('Invalid value for situation');
-        }
-        
-        if (!Object.values(GenderUser).includes(gender)) {
-            throw new BadRequestException('Invalid value for gender of user');
-        }
-
-        if (!Object.values(RegistrationStatusUser).includes(registrationStatus)) {
-            throw new BadRequestException('Invalid value for registrationStatus');
-          }     
-        
-          const newUser = this.userManagementRepository.create({
-            name,
-            lastName,
-            situation,
-            paymentAmount,
-            nationalCode,
-            programs,
-            province,
-            gender,
-            registrationStatus
-        });
-
-        const addedUser = await this.userManagementRepository.save(newUser);
-
-        return addedUser;
-        
-    } catch (err) {
-        throw new InternalServerErrorException('Internal Server Error');
+        throw new BadRequestException("Invalid value for situation");
       }
+
+      if (!Object.values(GenderUser).includes(gender)) {
+        throw new BadRequestException("Invalid value for gender of user");
+      }
+
+      if (!Object.values(RegistrationStatusUser).includes(registrationStatus)) {
+        throw new BadRequestException("Invalid value for registrationStatus");
+      }
+
+      const newUser = this.userManagementRepository.create({
+        name,
+        lastName,
+        situation,
+        paymentAmount,
+        nationalCode,
+        programs,
+        province,
+        gender,
+        registrationStatus,
+      });
+
+      const addedUser = await this.userManagementRepository.save(newUser);
+
+      return addedUser;
+    } catch (err) {
+      throw new InternalServerErrorException("Internal Server Error");
     }
+  }
 
   // getAllUsers
-    public async getManagementAllUsers(): Promise<UserManagementEntity[]> {
-      
+  public async getManagementAllUsers(): Promise<UserManagementEntity[]> {
     try {
-        const allUsers = await this.userManagementRepository.find({
-            order: {
-                registeryUser: 'DESC'
-            },
-            take: 10,
-        });
+      const allUsers = await this.userManagementRepository.find({
+        order: {
+          registeryUser: "DESC",
+        },
+        take: 10,
+      });
 
-        if (!allUsers || allUsers.length === 0) {
-            throw new NotFoundException('No users found')
-        }
+      if (!allUsers || allUsers.length === 0) {
+        throw new NotFoundException("No users found");
+      }
 
-        return allUsers;
+      return allUsers;
     } catch (err) {
-        throw new InternalServerErrorException('Failed to retrieve users');
+      throw new InternalServerErrorException("Failed to retrieve users");
+    }
+  }
+
+  //update user of client
+  public async updateManagementUser(id: string,userDto: UserManagementDto): Promise<UserManagementEntity | any> {
+    try {
+      const {
+        name,
+        lastName,
+        situation,
+        paymentAmount,
+        nationalCode,
+        programs,
+        province,
+        gender,
+        registrationStatus,
+      } = userDto;
+
+      const existingUser = await this.userManagementRepository.findOneBy({
+        id,
+      });
+
+      if (!existingUser) {
+        throw new Error(`user wwit ${id} not Found`);
+      }
+
+        const updatedUser = await this.userManagementRepository
+            .createQueryBuilder()
+            .update(UserManagementEntity)
+            .set({
+                name,
+                lastName,
+                situation,
+                paymentAmount,
+                nationalCode,
+                programs,
+                province,
+                gender,
+                registrationStatus,
+            })
+            .where({id })
+            .execute();
+        
+      if (updatedUser.affected === 0) {
+          throw new Error(`Failed to update user with ID ${id}`);
+      }
+        return updatedUser;
+    } catch (err) {
+        throw new Error(`Failed to update user: ${err.message}`);
     }
   }
 }
