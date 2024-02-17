0f736dc9da5c15c74862979256adb7377f72f465
hadi dasti <hadidasti98@hotmail.com>
2024-02-17 17:06:10 +0330

    add registerUserFortheProgram module in dashboard module

--- src/client/dashboard-client/dashboard.client.module.ts
+++ src/client/dashboard-client/dashboard.client.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { RegistrationManagementModule } from './registration-management/registration.management.module';
 import { ProgramManagementModule } from './program-management/program.management.module';
 import { UserManagementModule } from './user-management/user.management.module';
+import { RegisterUserForTheProgramModule } from "./register-user-for-program/register.user.for.program.module";
 
 
 @Module({
   imports: [
     RegistrationManagementModule,
     ProgramManagementModule,
-    UserManagementModule
+    UserManagementModule,
+    RegisterUserForTheProgramModule,
   ],
 })
 export class DashboardClientModule {}
